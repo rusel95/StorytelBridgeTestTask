5d23072a080a9ed7841c133c040b1667e38ec92f
Admin <ruslanpopesku95@gmail.com>
2017-05-19 17:15:32 +0300

    pagination downloading

--- StorytelBridgeTestTask/ListTableViewController.swift
+++ StorytelBridgeTestTask/ListTableViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ListTableViewController: UITableViewController {
-
+    
     private var neededPage = 0
     
     @IBOutlet weak var coverImage: UIImageView!
@@ -18,7 +18,7 @@ class ListTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        getCurrentList()
+        getNeededList()
         
     }
     
@@ -30,23 +30,32 @@ class ListTableViewController: UITableViewController {
         }
     }
     
-    private func getCurrentList() {
+    private func getNeededList() {
         StorytelBridgeApi.shared.getList(on: neededPage) { list in
             if list != nil {
-                self.currentList = list!
+                if self.neededPage > 0 {
+                    
+                    let tempList = self.currentList
+                    for book in (list?.books)! {
+                        tempList.books.append(book)
+                    }
+                    self.currentList = tempList
+                    print(self.currentList.books.count, self.neededPage)
+                } else {
+                    self.currentList = list!
+                }
                 self.neededPage += 1
             } else {
-                print("alert about fall down")
+                print("alert is needed - looks list is nil - no result")
             }
         }
     }
     
     private func setView(with list: List) {
         coverImage.setImage(with: list.coverURL)
-       
         titleLabel.text = list.title
     }
-
+    
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 130
     }
@@ -54,13 +63,19 @@ class ListTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return currentList.books.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "BookCell", for: indexPath) as! BookTableViewCell
         
         cell.setCell(with: currentList.books[indexPath.row])
-
+        
+        if indexPath.row == currentList.books.count - 1 {
+            getNeededList()
+        }
+        
         return cell
     }
+    
+    
 }
